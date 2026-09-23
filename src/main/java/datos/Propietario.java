34267efc7cfdbf73bef8679f09a9184518c38949
davdmc <harolddahitoht@gmail.com>
2026-09-22 20:03:40 -0500

    "se agrego el apartado de propietarios"

---
+++ src/main/java/datos/Propietario.java
@@ -0,0 +1,27 @@
+package datos;
+
+public class Propietario {
+    private String nombre;
+    private int identificacion;
+
+    public Propietario(String nombre, int identificacion) {
+        this.nombre = nombre;
+        this.identificacion = identificacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(int identificacion) {
+        this.identificacion = identificacione;
+    }
+}
